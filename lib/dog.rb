73c5da49d42e4e3b39994cbbb3e3fe20a1234080
Chris Allen <chrisallen415@gmail.com>
2020-05-19 16:11:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,15 @@
 class Dog
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(name)
+    @name = name
   end
 
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
+  def name=(name)
+    @name = first_name
   end
 
   def name
-    "#{@first_name} #{@last_name}".strip
+    @name
   end
 
 end
